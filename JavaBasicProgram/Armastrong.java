6cb7b742040ef1a0d2bf2351ee9d258e8509e1d7
0sacchidanand <sacchidanandlinge@gmail.com>
2023-09-12 00:21:55 +0530

    minor

--- JavaBasicProgram/Armastrong.java
+++ JavaBasicProgram/Armastrong.java
@@ -1,18 +1,17 @@
 public class Armastrong {
     public static void main(String[] args) {
-        int num=153,result=0,rem;
-        int temp=num;
-        while(num>0){
-            rem=num%10;
-            num=num/10;
-            result=result + (rem*rem*rem);
+        int num = 153, result = 0, rem;
+        int temp = num;
+        while (num > 0) {
+            rem = num % 10;
+            num = num / 10;
+            result = result + (rem * rem * rem);
         }
-        if(result==temp){
+        if (result == temp) {
             System.out.println("Armstrong Number");
-        }
-        else{
-            System.out.println("not a Armstrong Number");
+        } else {
+            System.out.println("not a Armstrong Number is");
         }
     }
-    
+
 }
